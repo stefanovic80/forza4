b0dea71c6a1ad05b0b806b93772082d0a8e50336
stefanovic80 <stefano.fedele@ucdconnect.ie>
2021-12-29 22:36:56 +0100

    division of red (even) and blue (odd) is provided. Also a function which figure out indexes is added.

--- forza4.py
+++ forza4.py
@@ -1,12 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-This is a temporary script file.
-"""
-
-#import pandas as pd
-#import os
 import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib.mlab as mlab
@@ -16,23 +7,6 @@ import numpy as np
 plt.close()
 plt.ion()
 
-"""
-matplotlib.rc('xtick', labelsize=14) 
-matplotlib.rc('ytick', labelsize=14) 
-
-plt.rcParams['lines.linewidth'] = 3
-#plt.rcparam['axes.grid'] = True 
-"""
-
-import warnings
-warnings.filterwarnings('ignore')
-
-font = {'family' : 'normal',
-        'weight' : 'bold',
-        'size'   : 14}
-
-matplotlib.rc('font', **font)
-
 fig = plt.figure(figsize = (5, 3))
 ax = fig.add_subplot(111)
 ax.set_xlim(.5, 6.5)
@@ -41,13 +15,32 @@ for u in range(0, 6, 1):
     ax.vlines(u + .5, -.5, 6.5)
     ax.hlines(u + .5, -.5, 7)
 
+#https://thispointer.com/python-how-to-find-all-indexes-of-an-item-in-a-list/
+def get_index_positions(list_of_elems, element):
+    ''' Returns the indexes of all occurrences of give element in
+    the list- listOfElements '''
+    index_pos_list = []
+    index_pos = 0
+    while True:
+        try:
+            # Search for item in list from indexPos to the end of list
+            index_pos = list_of_elems.index(element, index_pos)
+            # Add the index position in list
+            index_pos_list.append(index_pos)
+            index_pos += 1
+        except ValueError as e:
+            break
+    return index_pos_list
+
+
+
 r = 0
 coordX = [] #initializate x coordinate of each circle
 coordY = [] #initializate y coordinate of each circle
 colors = ['red', 'blue'] # colors of circles
 y = [1 for k in range(6)] #initializate y position of each circles
 
-while r < 19:   
+while r < 7:   
     coord = plt.ginput(n = 1)[0][0] #it takes x axis (second [0]) on forza4
     coord = np.round(coord)# it rounds to the closest integer
     coordX = coordX + [int(coord)]#list of x coordinates
@@ -58,14 +51,21 @@ while r < 19:
     
     print(j)
      
-    coordY = coordY + [y[j]]
+    coordY = coordY + [y[j]]#list of y coordinates
     dots, = ax.plot(coordX[r], y[j],\
                            'o', markersize=22, color = colors[r%2])
     plt.ginput(n = 1) #click two times before circle appearing
     r = r + 1
-    #print(y)
+    
     odd_blue = [y for x,y in enumerate(coordX) if x%2 != 0]
     odd_blueY = [y for x,y in enumerate(coordY) if x%2 != 0]
     
     even_red = [y for x,y in enumerate(coordX) if x%2 == 0]
     even_redY = [y for x,y in enumerate(coordY) if x%2 == 0]
+    
+    #for t in range(6):
+    t = 2
+    print(get_index_positions(odd_blue, t) )
+    print(get_index_positions(odd_blueY, t) )
+    print(get_index_positions(even_red, t))
+    print(get_index_positions(even_redY, t))
