38d963aa6393b9b4f7a172fafaf87cea7a02b3cf
stefanovic80 <stefano.fedele@ucdconnect.ie>
2021-12-30 12:04:35 +0100

    A much more efficient loop to figure out when red wins. Still it doesn't work for y values greater than 2. Debug is needed!

--- forza4.py
+++ forza4.py
@@ -15,24 +15,6 @@ for u in range(0, 6, 1):
     ax.vlines(u + .5, -.5, 6.5)
     ax.hlines(u + .5, -.5, 7)
 
-#https://thispointer.com/python-how-to-find-all-indexes-of-an-item-in-a-list/
-def get_index_positions(list_of_elems, element):
-    ''' Returns the indexes of all occurrences of give element in
-    the list- listOfElements '''
-    index_pos_list = []
-    index_pos = 0
-    while True:
-        try:
-            # Search for item in list from indexPos to the end of list
-            index_pos = list_of_elems.index(element, index_pos)
-            # Add the index position in list
-            index_pos_list.append(index_pos)
-            index_pos += 1
-        except ValueError as e:
-            break
-    return index_pos_list
-
-
 
 r = 0
 coordX = [] #initializate x coordinate of each circle
@@ -40,7 +22,8 @@ coordY = [] #initializate y coordinate of each circle
 colors = ['red', 'blue'] # colors of circles
 y = [1 for k in range(6)] #initializate y position of each circles
 
-while r < 17:   
+breakOuterLoop = 0
+while breakOuterLoop == 0:   
     coord = plt.ginput(n = 1)[0][0] #it takes x axis (second [0]) on forza4
     coord = np.round(coord)# it rounds to the closest integer
     coordX = coordX + [int(coord)]#list of x coordinates
@@ -48,8 +31,6 @@ while r < 17:
     j = coordX[-1] - 1 # j is the index of a list and starts from 0 (not from 1)
     if coordX[-1] in coordX[:-1]:   
         y[j] = y[j] + 1
-    
-    print(j)
      
     coordY = coordY + [y[j]]#list of y coordinates
     dots, = ax.plot(coordX[r], y[j],\
@@ -57,27 +38,31 @@ while r < 17:
     plt.ginput(n = 1) #click two times before circle appearing
     r = r + 1
     
-    odd_blue = [y for x,y in enumerate(coordX) if x%2 != 0]
-    odd_blueY = [y for x,y in enumerate(coordY) if x%2 != 0]
     
     even_red = [y for x,y in enumerate(coordX) if x%2 == 0]
     even_redY = [y for x,y in enumerate(coordY) if x%2 == 0]
-    
-    #for t in range(6):
-    t = 2
-    #while even_redY < 3:
-    #[a, a + 1, a + 2, a + 3]
-    print(get_index_positions(odd_blue, t) )
-    print(get_index_positions(odd_blueY, t) )
-    print(get_index_positions(even_red, t))
-    print(get_index_positions(even_redY, t))
-    
-    indRedY = get_index_positions(even_redY, t) #indexes red Y
-    if len(indRedY)> 3:
-        k = np.array(even_red)[indRedY].tolist()
+
+    odd_blue = [y for x,y in enumerate(coordX) if x%2 != 0]
+    odd_blueY = [y for x,y in enumerate(coordY) if x%2 != 0]
+
+    t = 0
+
+    loop_end = max(even_redY) + 1
+    while t < loop_end:
+        k = [b for a, b in zip(even_redY, even_red) if a == t]
+        #k = np.array(even_red)[indRedY].tolist() # select only
         k.sort()
-        g = min(k)
-        if k == [g, g + 1, g +2, g + 3]:
-            print("The winner is Red")
-            break
+        print(t)
+        print(loop_end)
+        try:
+            g = min(k)
+            if (len(k)> 3) and (k == [g, g + 1, g +2, g + 3]):
+                print("The winner is " + colors[0])
+                breakOuterLoop = 1
+                break
+        
+        except:
+            pass
+        
+        t +=1
         
